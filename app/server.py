6e893cbcb9b2368597d15c74ea2b6b80747c762d
Xu X <47133268+diablo34@users.noreply.github.com>
2019-08-18 21:03:59 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/open?id=1xelTams-smpLjHWaHsYuYG4mZkLl7GdB'
+export_file_url = 'https://drive.google.com/open?id=1e2_NS3ZQVUl3vnPPvL52ckaAe4sJTvF4'
 export_file_name = 'export.pkl'
 
 classes = ['HI~ 100% Natual Beauty~', 'Um....Plastic Face']
